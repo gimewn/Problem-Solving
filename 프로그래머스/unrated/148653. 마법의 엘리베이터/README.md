6335ad04adc9eb253104cf2a7c5371f955a6b10a
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-02-03 18:41:28 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/README.md
@@ -1,6 +1,6 @@
 # [unrated] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653#) 
 
 ### 성능 요약
 
--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -7,13 +7,18 @@ def solution(storey):
         now = storey % 10
         storey //= 10
         
+        # 맨 끝 수가 5보다 작으면 - 버튼 누르기
         if now < 5:
             answer += now
+        # 맨 끝 수가 5와 같으면
         elif now == 5:
             next = storey % 10
-            if next > 4:
+            # 다음 수가 5 이상일 경우 -> + 버튼 누르고, 그 다음 끝 수에 +1
+            # 다음 수가 5 미만일 경우 -> - 버튼 누르기
+            if next >= 5:
                 storey += 1
-            answer += now
+            answer += 5
+        # 맨 끝 수가 5보다 크면 + 버튼 누르고, 그 다음 끝 수에 +1
         else:
             answer += 10 - now
             storey += 1

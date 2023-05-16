47219227f76632a561c7595433f28e38e1bebb7e
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-05-16 12:07:39 +0900

    [Gold V] Title: 틱택토, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.py
+++ 백준/Gold/7682. 틱택토/틱택토.py
@@ -19,9 +19,8 @@ while True:
         if x > 2:
             y += 1
             x = 0
-    if x_cnt < 3 and o_cnt < 3:
-        print('invalid')
-    elif x_cnt - o_cnt == 1 or x_cnt == o_cnt:
+    # X가 O보다 1개 더 많거나 같을 경우만 valid가 나올 수 있음
+    if x_cnt - o_cnt == 1 or x_cnt == o_cnt:
         # X와 O의 이긴 횟수 세기
         win_cnt = {'X': 0, 'O': 0}
         # X와 O의 가로, 세로, 대각선 검사 =>
@@ -47,11 +46,13 @@ while True:
             win_cnt['O'] += 1
 
         flag = False
-
+        # O가 승리했고 X가 승리하지 않았을 경우 => X와 O의 개수가 동일해야 함
         if win_cnt['O'] and not win_cnt['X'] and x_cnt == o_cnt:
             flag = True
+        # X가 승리했고 O가 승리하지 않았을 경우 => X가 O보다 1개 더 많아야 함
         elif win_cnt['X'] and not win_cnt['O'] and x_cnt > o_cnt:
             flag = True
+        # 둘 다 승리하지 않았을 경우 => X가 O보다 1개 더 많아야 하고, 보드가 꽉 찬 상태여야 하므로 둘의 합이 9여야 함
         elif not win_cnt['X'] and not win_cnt['O'] and x_cnt > o_cnt and x_cnt + o_cnt == 9:
             flag = True
 

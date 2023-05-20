c410903a448f08e296403d04e2370e395e37084a
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-05-20 23:55:14 +0900

    [Bronze III] Title: ZOAC 4, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
@@ -2,7 +2,14 @@ import math
 
 H, W, N, M = map(int, input().split())
 
-cols = math.ceil(W/(M+1))
-rows = math.ceil(H/(N+1))
+if H % (N+1):
+    rows = H // (N+1) + 1
+else:
+    rows = H // (N+1)
+
+if W % (M+1):
+    cols = W // (M+1) + 1
+else:
+    cols = W // (M+1)
 
 print(cols*rows)

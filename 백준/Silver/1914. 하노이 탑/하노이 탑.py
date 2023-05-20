fc26f5aed8aac2af0cbbe11d19ca87aa528d3d9b
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-05-21 06:41:18 +0900

    [Silver I] Title: 하노이 탑, Time: 852 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.py
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -4,9 +4,9 @@ def hanoi(num, start, destination, other):
     if num == 1:
         print(start, destination)
         return
-    hanoi(num-1, start, other, destination) # n-1개의 원반을
-    print(start, destination) # n
-    hanoi(num-1, other, destination, start)
+    hanoi(num-1, start, other, destination) # num-1개의 원반을 2번으로 옮기기
+    print(start, destination) # num번 원반을 3번으로 옮기기
+    hanoi(num-1, other, destination, start) # num-1개의 원반을 3번으로 옮기기
 
 print(2**n-1)
 if n <= 20:

d31370d8a54e16f31640db215ff7bf17ce1d3abd
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-05-22 10:59:18 +0900

    [Gold IV] Title: 문자열 폭발, Time: 568 ms, Memory: 42436 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 words = input()
 bomb = list(input())
 
@@ -9,15 +7,23 @@ stack = []
 stack_len = 0
 
 for idx in range(len(words)):
+    # words의 문자열을 하나씩 stack에 담아준다.
     stack.append(words[idx])
+    # stack의 길이 +1
     stack_len += 1
+    # stack의 길이가 폭탄의 길이 이상이면
     if stack_len >= bomb_len:
+        # 끝에서부터 폭탄 길이만큼 뽑아와서 폭탄과 같은지 검사
         if stack[stack_len-bomb_len:] == bomb:
+            # 같다면 폭탄 길이만큼 pop해준다.
             for _ in range(bomb_len):
                 stack.pop()
+            # stack 길이에서 폭탄 길이만큼 빼준다.
             stack_len -= bomb_len
 
+# stack에 문자열이 남아 있으면 출력
 if stack:
     print(''.join(stack))
+# 남아 있지 않으면 FRULA 출력
 else:
     print("FRULA")

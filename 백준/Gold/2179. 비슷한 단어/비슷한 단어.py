bff26da09c4ba7c7bf64a4679f2caf82a8d0411a
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-05-25 11:21:32 +0900

    [Gold IV] Title: 비슷한 단어, Time: 148 ms, Memory: 42096 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.py
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.py
@@ -1,40 +1,32 @@
 import sys
 
-def cal(one, two):
-    global answer
-    if len(one) <= len(two):
-        limit = len(one)
-        target = two
-        start = one
-    elif len(two) < len(one):
-        limit = len(two)
-        target = one
-        start = two
-
-    for idx in range(limit, 0, -1):
-        if idx <= answer:
-            return False
-        if target.startswith(start[:idx]):
-            if idx > answer:
-                answer = idx
-                return idx
-
 n = int(sys.stdin.readline())
-answer = -1
-S, T = '', ''
 
-words = []
-ans_words = []
+word_dict = {}
 
 for _ in range(n):
     word = sys.stdin.readline().rstrip()
-    words.append(word)
+    for idx in range(len(word), 0, -1):
+        temp = word[:idx]
+        if idx in word_dict:
+            if temp in word_dict[idx]:
+                word_dict[idx][temp].append(word)
+            else:
+                word_dict[idx][temp] = [word]
+        else:
+            word_dict[idx] = {
+                temp: [word]
+            }
 
-for i in range(n-1):
-    for j in range(i+1, n):
-        res = cal(words[i], words[j])
-        if res:
-            ans_words = (words[i], words[j])
+word_dict_key = sorted(list(word_dict.keys()), reverse=True)
+flag = False
 
-print(ans_words[0])
-print(ans_words[1])
+for length in word_dict_key:
+    for w in word_dict[length]:
+        if len(word_dict[length][w]) >= 2:
+            print(word_dict[length][w][0])
+            print(word_dict[length][w][1])
+            flag = True
+            break
+    if flag:
+        break

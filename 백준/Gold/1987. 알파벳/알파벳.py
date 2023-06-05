c7b194dc7095fbc0d1fd08d333d02ce1999b7ebf
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-05 11:47:36 +0900

    [Gold IV] Title: 알파벳, Time: 1216 ms, Memory: 51732 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -0,0 +1,25 @@
+import sys
+
+dir = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+def BFS():
+    q = set()
+    q.add((0, 0, board[0][0]))
+    max_cnt = 0
+
+    while q:
+        ny, nx, nlist = q.pop()
+        max_cnt = max(max_cnt, len(nlist))
+
+        for i, j in dir:
+            dy, dx = ny+i, nx+j
+            if 0 <= dy < R and 0 <= dx < C and board[dy][dx] not in nlist:
+                q.add((dy, dx, nlist+board[dy][dx]))
+
+    return max_cnt
+
+R, C = map(int, sys.stdin.readline().split())
+
+board = [sys.stdin.readline().rstrip() for _ in range(R)]
+
+print(BFS())

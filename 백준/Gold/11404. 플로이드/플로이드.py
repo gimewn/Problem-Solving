f6b48b33d3341f60bd20174e281f6bc01d2345b5
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-06 22:55:44 +0900

    [Gold IV] Title: 플로이드, Time: 404 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,44 +1,38 @@
-from sys import stdin
-import heapq
+# 플로이드 워셜 풀이
 
-city_cnt = int(stdin.readline())
-bus_cnt = int(stdin.readline())
+import sys
+
+city_cnt = int(sys.stdin.readline())
+bus_cnt = int(sys.stdin.readline())
 
 city = [[2e9]*city_cnt for _ in range(city_cnt)]
 
-bus = {}
+def make_myself_zero(cnt):
+    # 자기 자신으로 가는 경우를 0으로 갱신
+    for y in range(cnt):
+        city[y][y] = 0
+
+def floyd_warshall(cnt):
+    # i번째 도시를 경유하는 경우
+    for i in range(cnt):
+        for j in range(cnt):
+            for k in range(cnt):
+                # j에서 k로 바로 가는 것 VS j -> i -> k 로 i를 경유하는 것 비교
+                city[j][k] = min(city[j][k], city[j][i] + city[i][k])
 
 for _ in range(bus_cnt):
-    start, destination, cost = map(int, stdin.readline().split())
-    start, destination = start-1, destination-1
-    if start not in bus:
-        bus[start] = {}
-    if destination not in bus[start]:
-        bus[start][destination] = 2e9
-    bus[start][destination] = min(bus[start][destination], cost)
+    a, b, c = map(int, sys.stdin.readline().split())
+    # 시작 도시와 도착 도시를 연결하는 노선 중 최소 비용
+    city[a-1][b-1] = min(city[a-1][b-1], c)
 
-def dijkstra(bus_num):
-    heap = []
-    heapq.heappush(heap, (bus_num, 0))
-    city[bus_num][bus_num] = 0
+make_myself_zero(city_cnt)
 
-    while heap:
-        now, now_cost = heapq.heappop(heap)
-        if now in bus:
-            for next_value in bus[now]:
-                next_cost = bus[now][next_value]
-                if city[bus_num][next_value] > now_cost + next_cost:
-                    city[bus_num][next_value] = now_cost + next_cost
-                    heapq.heappush(heap, (next_value, city[bus_num][next_value]))
+floyd_warshall(city_cnt)
 
-for idx in range(city_cnt):
-    if idx in bus:
-        dijkstra(idx)
-
-for i in range(city_cnt):
-    for j in range(city_cnt):
-        if city[i][j] == 2e9:
-            print(0, end=" ")
+for y in range(city_cnt):
+    for x in range(city_cnt):
+        if city[y][x] < 2e9:
+            print(city[y][x], end=" ")
         else:
-            print(city[i][j], end=" ")
+            print(0, end=" ")
     print()

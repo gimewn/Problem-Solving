bbb9f8a88c484388afe73d31900920ea55fce210
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-07 23:28:44 +0900

    [Gold V] Title: 평범한 배낭, Time: 3380 ms, Memory: 375892 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,5 +1,17 @@
 import sys
 
+def check_value():
+    for i in range(N):
+        for j in range(K + 1):
+            if j < items[i][0]:
+                bags[i][j] = bags[i - 1][j]
+            else:
+                # bags[i-1][j-items[i][0]]
+                # bags[i][j-items[i][0]]일 경우, 자기 자신을 2번 더할 수 있음
+                # 6 => 3 3
+                # items를 정렬해주고, bags[i-1]을 참조함
+                bags[i][j] = max(bags[i - 1][j], bags[i - 1][j - items[i][0]] + items[i][1])
+
 N, K = map(int, sys.stdin.readline().split())
 
 bags = [[0]*(K+1) for _ in range(N)]
@@ -12,11 +24,6 @@ for _ in range(N):
 
 items.sort()
 
-for i in range(N):
-    for j in range(K+1):
-        if j < items[i][0]:
-            bags[i][j] = bags[i-1][j]
-        else:
-            bags[i][j] = max(bags[i-1][j], bags[i-1][j-items[i][0]] + items[i][1])
+check_value()
 
 print(bags[-1][-1])

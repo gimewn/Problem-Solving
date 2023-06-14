535bd8c638e9d7e731ce214f39bc9f838e6a003d
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-14 23:24:38 +0900

    [Gold IV] Title: 행성 연결, Time: 1860 ms, Memory: 123180 KB -BaekjoonHub

--- 백준/Gold/16398. 행성 연결/행성 연결.py
+++ 백준/Gold/16398. 행성 연결/행성 연결.py
@@ -5,37 +5,28 @@ sys.setrecursionlimit(10**5)
 
 def main():
 
-    def find(num):
-        if parents[num] == num:
-            return num
-        parents[num] = find(parents[num])
-        return parents[num]
+    def prim(start):
+        answer = 0
+        heap = []
+        heapq.heappush(heap, (0, start))
+        while heap:
+            cost, num = heapq.heappop(heap)
+            if not check[num]:
+                check[num] = 1
+                answer += cost
+                for i in range(N):
+                    if num != i and not check[i]:
+                        heapq.heappush(heap, (board[num][i], i))
+        return answer
 
-    def union(n1, n2):
-        min_, max_ = min(n1, n2), max(n1, n2)
-        parents[max_] = min_
 
     N = int(sys.stdin.readline())
 
     planets = []
-    parents = [num for num in range(N)]
+    check = [0]*N
+    board = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-    answer = 0
-
-    for y in range(N):
-        lst = list(map(int, sys.stdin.readline().split()))
-        for x in range(N):
-            if y != x:
-                heapq.heappush(planets, (lst[x], y, x))
-
-    while planets:
-        cost, y, x = heapq.heappop(planets)
-        fy, fx = find(y), find(x)
-        if fy != fx:
-            union(fy, fx)
-            answer += cost
-
-    print(answer)
+    print(prim(0))
 
 if __name__ == '__main__':
     main()

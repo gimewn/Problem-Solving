3470a9d475bddc3ba1e3767b4fbe241884663616
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-15 11:22:54 +0900

    [Gold IV] Title: 서강그라운드, Time: 164 ms, Memory: 115520 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.py
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.py
@@ -22,12 +22,11 @@ for i in range(1, N+1):
     for j in range(1, N+1):
         for k in range(1, N+1):
             board[j][k] = min(board[j][k], board[j][i] + board[i][k])
+            # 수색 범위 내에 있으면 아이템 줍기
+            if board[j][k] <= M:
+                cal_itemes[j][k] = items[k]
 
-for y in range(1, N+1):
-    temp = 0
-    for x in range(1, N+1):
-        if board[y][x] <= M:
-            temp += items[x]
-    answer = max(answer, temp)
+for idx in range(1, N+1):
+    answer = max(answer, sum(cal_itemes[idx]))
 
 print(answer)

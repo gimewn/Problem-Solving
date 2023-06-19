2f76999a6317eee33a0cf76dbde173aed900951e
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-19 10:58:31 +0900

    [Gold IV] Title: 탈출, Time: 64 ms, Memory: 34264 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.py
+++ 백준/Gold/3055. 탈출/탈출.py
@@ -9,21 +9,26 @@ def water_bfs(q):
         for i, j in dir:
             dy, dx = ny+i, nx+j
             if 0 <= dy < R and 0 <= dx < C:
+                # 돌이 없고, 비버의 집이 아니며, 물이 아직 안 차오른 곳이면
                 if board[dy][dx] != 'X' and board[dy][dx] != 'D' and water[dy][dx] < 0:
                     water[dy][dx] = water[ny][nx] + 1
                     q.append((dy, dx))
+
 def escape_bfs(s, e):
     q = deque()
     q.append((s[0], s[1], 0))
     visit = [[0]*C for _ in range(R)]
     visit[s[0]][s[1]] = 1
+
     while q:
         ny, nx, ncnt = q.popleft()
+        # 위치가 비버의 집이면 방문한 시간 return
         if ny == e[0] and nx == e[1]:
             return ncnt
         for i, j in dir:
             dy, dx = ny+i, nx+j
             if 0 <= dy < R and 0 <= dx < C:
+                # 방문한 적 없고, 돌이 없으며, 물이 아직 안 차올랐거나 차오를 일 없으면
                 if not visit[dy][dx] and board[dy][dx] != 'X' and (water[dy][dx] > ncnt+1 or water[dy][dx] == -1):
                     visit[dy][dx] = 1
                     q.append((dy, dx, ncnt+1))
@@ -34,9 +39,13 @@ R, C = map(int, sys.stdin.readline().split())
 
 board = []
 
+# 고슴도치 위치
 start = 0, 0
+# 비버 위치
 end = 0, 0
+# 물 초기 위치
 water_position = deque()
+# 물이 차오를 시기 기록
 water = [[-1]*C for _ in range(R)]
 
 for y in range(R):

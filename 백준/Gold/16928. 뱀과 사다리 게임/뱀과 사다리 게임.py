02cb5a9975a30ab5cf4b555026200c6d21ad85d4
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-20 23:57:12 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 64 ms, Memory: 34232 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -1,44 +1,68 @@
 import sys
 from collections import deque
 
-def BFS():
+def cal_index(num):
+    y = num // 10 if num % 10 > 0 else num // 10 - 1
+    x = num % 10 - 1 if num % 10 > 0 else 9
+    return (y, x)
+
+def ladder_or_snake(dictionary, limit):
+    for _ in range(limit):
+        y, x = map(int, sys.stdin.readline().split())
+        dictionary[y] = x
+
+def bfs():
     q = deque()
     q.append((1, 0))
-    visit = [0]*101
-    visit[1] = 1
+    visit = [[2e9]*10 for _ in range(10)]
+    visit[0][0] = 0
+
     while q:
         now, cnt = q.popleft()
+        
+        ny, nx = cal_index(now)
+        
+        if visit[ny][nx] < cnt:
+            ncnt = visit[ny][nx]
+        else:
+            ncnt = cnt
+            
         if now == 100:
-            return cnt
-        for num in range(1, 7):
-            tmp = now+num
-            if tmp <= 100:
-                if visit[tmp]: continue
-                if tmp in ladder.keys():
-                    visit[tmp] = 1
-                    visit[ladder[tmp]] = 1
-                    q.append((ladder[tmp], cnt+1))
-                elif tmp in snake.keys():
-                    visit[tmp] = 1
-                    visit[snake[tmp]] = 1
-                    q.append((snake[tmp], cnt+1))
-                else:
-                    visit[tmp] = 1
-                    q.append((now+num, cnt+1))
+            return ncnt
 
-input = sys.stdin.readline
+        for dice in range(1, 7):
+            if now + dice <= 100:
+                dy, dx = cal_index(now+dice)
+                if visit[dy][dx] > ncnt+1:
+                    flag = 1
+                    if now + dice in ladder:
+                        ly, lx = cal_index(ladder[now + dice])
+                        if visit[ly][lx] == 2e9:
+                            visit[ly][lx] = ncnt+1
+                            q.append((ladder[now + dice], visit[ly][lx]))
+                        elif visit[ly][lx] > ncnt+1:
+                            visit[ly][lx] = ncnt+1
+                        flag = 0
+                        
+                    if now + dice in snake:
+                        sy, sx = cal_index(snake[now + dice])
+                        if visit[sy][sx] == 2e9:
+                            visit[sy][sx] = ncnt+1
+                            q.append((snake[now + dice], visit[sy][sx]))
+                        elif visit[sy][sx] > ncnt+1:
+                            visit[sy][sx] = ncnt+1
+                        flag = 0
+                        
+                    if flag:
+                        visit[dy][dx] = ncnt + 1
+                        q.append((now + dice, ncnt+1))
 
-N, M = map(int, input().split())
+N, M = map(int, sys.stdin.readline().split())
 
-ladder = {}
-snake = {}
+ladder = dict()
+snake = dict()
 
-for _ in range(N):
-    start, end = map(int, input().split())
-    ladder[start] = end
+ladder_or_snake(ladder, N)
+ladder_or_snake(snake, M)
 
-for _ in range(M):
-    start, end = map(int, input().split())
-    snake[start] = end
-
-print(BFS())
+print(bfs())

aba3358d1ec24afdbd5381ad2ff409a95c1910e9
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-23 10:01:15 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 96 ms, Memory: 42176 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.py
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.py
@@ -1,22 +1,31 @@
-n = int(input())
+import sys
 
-nums = list(map(int, input().split()))
-answer = 0
+def two_pointer():
+    answer = 0
+    end = 0
+
+    for start in range(N):
+        while end < N:
+            # 수열의 마지막 부분이 체크되어 있으면
+            if check[lst[end]]:
+                # 수열의 시작 부분을 체크 해제
+                check[lst[start]] = 0
+                break
+            else:
+                # 수열의 마지막 부분 체크
+                check[lst[end]] = 1
+                # end ++
+                end += 1
+                # end - start
+                # 만약 end가 3이고 start 가 0 (1 2 3)
+                # 3 / 32 / 321 => 3개
+                answer += end - start
+    return answer
+
+N = int(sys.stdin.readline())
+
+lst = list(map(int, sys.stdin.readline().split()))
+
 check = [0]*100001
 
-start, end = 0, 0
-
-while start < n and end < n:
-    if not check[nums[end]]:
-        check[nums[end]] = 1
-        end += 1
-        # 만약 end가 3이고 start가 0이라면
-        # 3 / 23 / 123 ... (3개
-        # end가 2이고 start가 0이면
-        # 2 / 12 (2개)
-        answer += end - start
-    else:
-        check[nums[start]] = 0
-        start += 1
-
-print(answer)
+print(two_pointer())

de4c11a994dde56ef5852f47df50eca07c97605a
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-23 10:52:06 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 232 ms, Memory: 53380 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
@@ -1,34 +1,23 @@
-N, K = map(int, input().split())
+import sys
 
-lst = list(map(int, input().split()))
+N, K = map(int, sys.stdin.readline().split())
 
-num_dict = dict()
+lst = list(map(int, sys.stdin.readline().split()))
 
 answer = 0
+end = 0
 
-start, end = 0, 0
+check = [0]*100001
 
-while end < N:
-    # lst[end] 와 lst[start]가 딕셔너리에 존재하지 않으면 등록
-    if lst[end] not in num_dict:
-        num_dict[lst[end]] = 0
+for start in range(N):
+    while end < N:
+        if check[lst[end]] >= K:
+            check[lst[start]] -= 1
+            break
 
-    if lst[start] not in num_dict:
-        num_dict[lst[start]] = 0
-    
-    # 만약 end인 숫자가 K개 이상이면 => start 당겨야 함
-    if num_dict[lst[end]] >= K:
-        # 기존 start 숫자의 등장횟수 감소시키기
-        num_dict[lst[start]] -= 1
-        # start 1 증가
-        start += 1
-    else:
-        # end인 숫자가 K개 미만이면
-        # end 숫자 등장 횟수 1 증가
-        num_dict[lst[end]] += 1
-        # answer 갱신
-        answer = max(answer, end-start+1)
-        # end 1 증가
-        end += 1
+        else:
+            check[lst[end]] += 1
+            end += 1
+            answer = max(answer, end - start)
 
 print(answer)

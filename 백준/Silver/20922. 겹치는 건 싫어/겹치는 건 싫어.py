c23594aa6e6edc8ae9e2698bf543622c27b54c7d
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-23 10:55:18 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 172 ms, Memory: 53380 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
@@ -1,23 +1,26 @@
 import sys
 
+def two_pointer():
+    answer = 0
+    end = 0
+
+    check = [0] * 100001
+
+    for start in range(N):
+        while end < N:
+            if check[lst[end]] >= K:
+                check[lst[start]] -= 1
+                break
+
+            else:
+                check[lst[end]] += 1
+                end += 1
+                answer = max(answer, end - start)
+
+    return answer
+
 N, K = map(int, sys.stdin.readline().split())
 
 lst = list(map(int, sys.stdin.readline().split()))
 
-answer = 0
-end = 0
-
-check = [0]*100001
-
-for start in range(N):
-    while end < N:
-        if check[lst[end]] >= K:
-            check[lst[start]] -= 1
-            break
-
-        else:
-            check[lst[end]] += 1
-            end += 1
-            answer = max(answer, end - start)
-
-print(answer)
+print(two_pointer())

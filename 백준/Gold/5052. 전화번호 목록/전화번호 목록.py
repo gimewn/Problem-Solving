fe53433aa051a03a69b8844f832cfb3939a61b9f
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-23 10:41:25 +0900

    [Gold IV] Title: 전화번호 목록, Time: 232 ms, Memory: 33324 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -7,6 +7,7 @@ for _ in range(t):
     n = int(sys.stdin.readline())
     answer = 1
     word = []
+    
     for _ in range(n):
         tel = sys.stdin.readline().rstrip()
         heapq.heappush(word, tel)
@@ -14,9 +15,13 @@ for _ in range(t):
     prev = ''
 
     while word:
+        # 사전순으로 빠른 것부터 하나씩 꺼내서
         now = heapq.heappop(word)
+        # 만약 prev가 있으면
         if prev:
+            # now가 prev로 시작하는지(접두어인지) 확인
             if now.startswith(prev):
+                # 접두어이면 answer 갱신, break
                 answer = 0
                 break
         prev = now

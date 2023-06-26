4e3ed2fd891a360c3ba2eb4d3c586b7b397bf12e
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-26 11:33:02 +0900

    [Gold IV] Title: 스도쿠, Time: 3592 ms, Memory: 151956 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,5 +1,6 @@
 import sys
 
+# 숫자 비워져 있는 칸 찾기
 def find_not_fill():
   lst = []
   for y in range(9):
@@ -8,18 +9,21 @@ def find_not_fill():
         lst.append((y, x))
   return lst
 
+# 해당 행에 들어 있는 숫자인지 확인
 def row_fill(Y, num):
     for x in range(9):
         if board[Y][x] == num:
             return True
     return False
 
+# 해당 열에 들어 있는 숫자인지 확인
 def col_fill(X, num):
     for y in range(9):
         if board[y][X] == num:
             return True
     return False
 
+# 해당 구역(3*3)에 들어 있는 숫자인지 확인
 def ground_fill(Y, X, num):
     gy, gx = Y // 3 * 3, X // 3 * 3
     for y in range(gy, gy + 3):
@@ -29,17 +33,25 @@ def ground_fill(Y, X, num):
     return False
 
 def find_num(idx):
+    # 인덱스가 not_fill의 길이와 같다 => 모든 빈 곳 다 채웠다
+    # 프린트하고 종료시키기
     if idx == len(not_fill):
         for i in range(9):
             print(*board[i])
         sys.exit()
-
+      
     for n in range(1, 10):
         ny, nx = not_fill[idx][0], not_fill[idx][1]
-        if not row_fill(ny, n) and not col_fill(nx, n) and not ground_fill(ny, nx, n):
-            board[ny][nx] = n
-            find_num(idx + 1)
-            board[ny][nx] = 0
+        if row_fill(ny, n):
+            continue
+        if col_fill(nx, n):
+            continue
+        if ground_fill(ny, nx, n):
+            continue
+            
+        board[ny][nx] = n
+        find_num(idx+1)
+        board[ny][nx] = 0
 
 
 inputs = sys.stdin.readline

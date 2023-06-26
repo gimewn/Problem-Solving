890690c5bc2d41c3dceb9bc27bf4eb92235113c5
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-26 17:49:47 +0900

    [Silver I] Title: IOIOI, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,23 @@
+import sys
+
+N = int(sys.stdin.readline())
+M = int(sys.stdin.readline())
+Pn = 'IO'*N + 'I'
+S = sys.stdin.readline().rstrip()
+
+start, end = 0, 2*N
+word = S[:end+1]
+answer = 0
+
+while True:
+    if word == Pn:
+        answer += 1
+    word = word[1:]
+    start += 1
+    end += 1
+    if end >= M:
+        break
+    word = word + S[end]
+
+
+print(answer)

0f7f0629062b950c2ec3e2740655e097e1368bb4
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-28 18:16:58 +0900

    [Gold V] Title: 문자열 지옥에 빠진 호석, Time: 84 ms, Memory: 34184 KB -BaekjoonHub

---
+++ 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+
+def do_bfs():
+    for y in range(N):
+        for x in range(M):
+            bfs(y, x)
+
+def bfs(sy, sx):
+    q = deque()
+    q.append((sy, sx, board[sy][sx]))
+
+    while q:
+        ny, nx, nword = q.popleft()
+
+        if nword in likes:
+            likes[nword] += 1
+
+        if len(nword) >= 5:
+            continue
+
+        for i, j in (-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (1, 1), (-1, 1), (1, -1):
+            dy, dx = (ny + i) % N, (nx + j) % M
+            q.append((dy, dx, nword + board[dy][dx]))
+
+
+inputs = sys.stdin.readline
+
+N, M, K = map(int, inputs().split())
+
+board = [list(inputs().rstrip()) for _ in range(N)]
+check = [[0]*M for _ in range(N)]
+
+likes = {}
+
+for _ in range(K):
+    word = inputs().rstrip()
+    likes[word] = 0
+
+do_bfs()
+
+for value in likes.values():
+    print(value)

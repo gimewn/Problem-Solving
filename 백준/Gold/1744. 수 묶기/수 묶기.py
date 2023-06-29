e81a7fc0139d2e1a8d54648b68bf4c3a1bd481aa
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-29 12:13:10 +0900

    [Gold IV] Title: 수 묶기, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -0,0 +1,52 @@
+import sys
+
+def find_partner(lst):
+    global answer
+    if lst:
+        while len(lst) >= 2:
+            one = lst.pop(0)
+            two = lst.pop(0)
+
+            answer += max(one * two, one + two)
+
+inputs = sys.stdin.readline
+
+N = int(inputs())
+
+plus = []
+minus = []
+
+answer = 0
+
+zero = 0
+
+for _ in range(N):
+    num = int(inputs())
+    if N == 1:
+        print(num)
+        exit()
+    if num > 0:
+        plus.append(num)
+    elif num == 0:
+        plus.append(num)
+        zero += 1
+    else:
+        minus.append(num)
+
+plus.sort(reverse=True)
+minus.sort()
+
+if len(minus) % 2 == 1 and zero:
+    zero -= 1
+    plus.pop()
+    minus.pop()
+
+find_partner(plus)
+find_partner(minus)
+left = plus+minus
+find_partner(left)
+
+if left:
+    answer += left[0]
+
+print(answer)

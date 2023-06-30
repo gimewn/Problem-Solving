76d0563ea3dc9cfb98a622bc1d43c26fd3f6ff6d
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-30 19:14:51 +0900

    [Gold IV] Title: 연구소, Time: 2348 ms, Memory: 34232 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -3,62 +3,65 @@ from collections import deque
 
 inputs = sys.stdin.readline
 
-def dfs(now, new_walls):
-    global min_virus
-    if len(new_walls) == 3:
-        min_virus = min(min_virus, check_safe(new_walls))
-        return
+def find_max_safe():
+    def dfs(now, new_walls):
+        nonlocal min_virus
+        if len(new_walls) == 3:
+            min_virus = min(min_virus, check_safe(new_walls))
+            return
 
-    for idx in range(now+1, empty_length):
-        dfs(idx, new_walls + [empty[idx]])
+        for idx in range(now+1, empty_length):
+            dfs(idx, new_walls + [empty[idx]])
 
-def check_safe(walls):
-    q = deque()
-    cnt = 3
-    visit = set()
+    def check_safe(walls):
+        q = deque()
+        cnt = 3
+        visit = set()
 
-    for vy, vx, vlst in virus:
-        q.append((vy, vx))
-        visit.add((vy, vx))
+        for vy, vx, vlst in virus:
+            q.append((vy, vx))
+            visit.add((vy, vx))
 
-    while q:
-        ny, nx = q.popleft()
-        if cnt >= min_virus:
-            return 2e9
+        while q:
+            ny, nx = q.popleft()
+            if cnt >= min_virus:
+                return 2e9
 
-        for i, j in (0, -1), (0, 1), (1, 0), (-1, 0):
-            dy, dx = ny + i, nx + j
-            if 0 <= dy < N and 0 <= dx < M and not board[dy][dx] and (dy, dx) not in walls and (dy, dx) not in visit:
-                visit.add((dy, dx))
-                cnt += 1
-                q.append((dy, dx))
+            for i, j in (0, -1), (0, 1), (1, 0), (-1, 0):
+                dy, dx = ny + i, nx + j
+                if 0 <= dy < N and 0 <= dx < M and not board[dy][dx] and (dy, dx) not in walls and (dy, dx) not in visit:
+                    visit.add((dy, dx))
+                    cnt += 1
+                    q.append((dy, dx))
 
-    return cnt
+        return cnt
 
-N, M = map(int, inputs().split())
+    N, M = map(int, inputs().split())
 
-board = []
-virus = []
-safe = N*M
-wall = 0
-empty = []
-min_virus = 2e9
+    board = []
+    virus = []
+    safe = N*M
+    wall = 0
+    empty = []
+    min_virus = 2e9
 
-for y in range(N):
-    lst = list(map(int, inputs().split()))
-    for x in range(M):
-        if lst[x] > 0:
-            safe -= 1
-            if lst[x] == 2:
-                virus.append((y, x, []))
-            elif lst[x] == 1:
-                wall += 1
-        else:
-            empty.append((y, x))
-    board.append(lst)
+    for y in range(N):
+        lst = list(map(int, inputs().split()))
+        for x in range(M):
+            if lst[x] > 0:
+                safe -= 1
+                if lst[x] == 2:
+                    virus.append((y, x, []))
+                elif lst[x] == 1:
+                    wall += 1
+            else:
+                empty.append((y, x))
+        board.append(lst)
 
-empty_length = len(empty)
+    empty_length = len(empty)
 
-dfs(-1, [])
+    dfs(-1, [])
 
-print(safe - min_virus)
+    return safe - min_virus
+
+print(find_max_safe())

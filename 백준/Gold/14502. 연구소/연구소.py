0f27f8bbfc90491878751706aae8841e6c857222
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-06-30 19:19:03 +0900

    [Gold IV] Title: 연구소, Time: 2048 ms, Memory: 34232 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -3,65 +3,66 @@ from collections import deque
 
 inputs = sys.stdin.readline
 
-def find_max_safe():
-    def dfs(now, new_walls):
-        nonlocal min_virus
-        if len(new_walls) == 3:
-            min_virus = min(min_virus, check_safe(new_walls))
-            return
+# 벽 세울 3곳 찾기
+def dfs(now, new_walls):
+    global min_virus
+    if len(new_walls) == 3:
+        # min_virus 갱신
+        min_virus = min(min_virus, check_safe(new_walls))
+        return
 
-        for idx in range(now+1, empty_length):
-            dfs(idx, new_walls + [empty[idx]])
+    for idx in range(now+1, empty_length):
+        dfs(idx, new_walls + [empty[idx]])
 
-    def check_safe(walls):
-        q = deque()
-        cnt = 3
-        visit = set()
+def check_safe(walls):
+    q = deque()
+    # cnt 초기값 = 벽 3개
+    cnt = 3
+    check = [[0]*M for _ in range(N)]
+    # virus 초기 세팅
+    for vy, vx, vlst in virus:
+        q.append((vy, vx))
+        check[vy][vx] = 1
 
-        for vy, vx, vlst in virus:
-            q.append((vy, vx))
-            visit.add((vy, vx))
+    while q:
+        ny, nx = q.popleft()
+        # cnt가 min_virus보다 커지면 더 확인 안 해도 됨
+        if cnt >= min_virus:
+            return 2e9
 
-        while q:
-            ny, nx = q.popleft()
-            if cnt >= min_virus:
-                return 2e9
+        for i, j in (0, -1), (0, 1), (1, 0), (-1, 0):
+            dy, dx = ny + i, nx + j
+            # board에서 0이고, 벽이 새로 세워진 위치가 아니고, 아직 방문하지 않았으면
+            if 0 <= dy < N and 0 <= dx < M and not board[dy][dx] and (dy, dx) not in walls and not check[dy][dx]:
+                check[dy][dx] = 1
+                cnt += 1
+                q.append((dy, dx))
 
-            for i, j in (0, -1), (0, 1), (1, 0), (-1, 0):
-                dy, dx = ny + i, nx + j
-                if 0 <= dy < N and 0 <= dx < M and not board[dy][dx] and (dy, dx) not in walls and (dy, dx) not in visit:
-                    visit.add((dy, dx))
-                    cnt += 1
-                    q.append((dy, dx))
+    return cnt
 
-        return cnt
+N, M = map(int, inputs().split())
 
-    N, M = map(int, inputs().split())
+board = []
+virus = []
+safe = N*M
+wall = 0
+empty = [] 
+min_virus = 2e9
 
-    board = []
-    virus = []
-    safe = N*M
-    wall = 0
-    empty = []
-    min_virus = 2e9
+for y in range(N):
+    lst = list(map(int, inputs().split()))
+    for x in range(M):
+        # 1이나 2면 => safe에서 빼주기
+        if lst[x] > 0:
+            safe -= 1
+            if lst[x] == 2:
+                virus.append((y, x, []))
+        else:
+            empty.append((y, x))
+    board.append(lst)
 
-    for y in range(N):
-        lst = list(map(int, inputs().split()))
-        for x in range(M):
-            if lst[x] > 0:
-                safe -= 1
-                if lst[x] == 2:
-                    virus.append((y, x, []))
-                elif lst[x] == 1:
-                    wall += 1
-            else:
-                empty.append((y, x))
-        board.append(lst)
+empty_length = len(empty)
 
-    empty_length = len(empty)
+dfs(-1, [])
 
-    dfs(-1, [])
-
-    return safe - min_virus
-
-print(find_max_safe())
+print(safe - min_virus)

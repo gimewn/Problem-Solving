20d7c29dc0159119a28408fc281ae8d0d8cd63e6
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-07-14 05:11:48 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.48 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,14 +1,17 @@
 import heapq
 
 def solution(n, wires):
+    # check 리셋
     def reset_check():
         return [0]*(n+1)
     
     def dijkstra(start, no_check):
+        # 출발점과 연결 끊은 곳은 미리 체크 => 들리지 않게
         check[no_check] = 1
         check[start] = 1
         heap = []
         heapq.heappush(heap, start)
+        # 출발점에서 갈 수 있는 곳의 개수 (같은 그룹)
         cnt = 1
         
         while heap:
@@ -23,11 +26,15 @@ def solution(n, wires):
     def find_group(one, two):
         nonlocal check
         check = reset_check()
+        # 1번 그룹 개수 찾기 (1번에서 갈 수 있는 곳들)
         group_one = dijkstra(one, two)
+        # 2번 그룹 개수 찾기 (2번에서 갈 수 있는 곳들)
         group_two = dijkstra(two, one)
         
+        # 1번 그룹 + 2번 그룹이 n일 때 => 그룹이 2개로 나뉘었다.
         if group_one + group_two == n:
             return abs(group_one - group_two)
+        # 1번 그룹 + 2번 그룹이 n보다 크거나 작을 때 => 그룹을 2개로 나눌 수 없다
         else:
             return 2e9
         

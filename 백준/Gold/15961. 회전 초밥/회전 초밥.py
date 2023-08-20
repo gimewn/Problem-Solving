82576b7e1bbe114609633bbb9ac5a519c7e95f98
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-08-20 23:38:02 +0900

    [Gold IV] Title: 회전 초밥, Time: 3152 ms, Memory: 141088 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.py
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.py
@@ -4,34 +4,33 @@ N, D, K, C = map(int, sys.stdin.readline().split())
 
 sushi_belt = [int(sys.stdin.readline()) for _ in range(N)]
 
-sushi_count = {}
+sushi_count = [0]*(D+1)
+
+sushi_count[C] = 1
 
 max_count = 0
 
 start = 0
 
+count = 1
+
 for idx in range(K-1):
-    if sushi_belt[idx] not in sushi_count:
-        sushi_count[sushi_belt[idx]] = 0
     sushi_count[sushi_belt[idx]] += 1
+    if sushi_count[sushi_belt[idx]] == 1 and sushi_belt[idx] != C:
+        count += 1
 
 for end in range(K-1, N+K-1):
     end = end % N
-    if sushi_belt[end] not in sushi_count:
-        sushi_count[sushi_belt[end]] = 0
     sushi_count[sushi_belt[end]] += 1
 
-    this_count = len(sushi_count.keys())
+    if sushi_count[sushi_belt[end]] == 1:
+        count += 1
 
-    if C not in sushi_count:
-        this_count += 1
+    max_count = max(max_count, count)
 
-    max_count = max(max_count, this_count)
-
+    if sushi_count[sushi_belt[start]] == 1:
+        count -= 1
     sushi_count[sushi_belt[start]] -= 1
-
-    if not sushi_count[sushi_belt[start]]:
-        del sushi_count[sushi_belt[start]]
 
     start += 1
 

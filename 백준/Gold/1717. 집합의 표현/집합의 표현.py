9371d0c73873f4c9267da057db6f0bc94785896a
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-08-21 16:15:14 +0900

    [Gold V] Title: 집합의 표현, Time: 304 ms, Memory: 71248 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,29 +1,25 @@
 import sys
-
 input = sys.stdin.readline
 
-def find(num):
-    if num == dot[num]:
-        return num
-    dot[num] = find(dot[num])
-    return dot[num]
+n, m = map(int, input().split())
+parents = [i for i in range(n + 1)]
 
-def union(type, n1, n2):
-    f1, f2 = find(n1), find(n2)
-    # type이 1이면 => 같은 집합에 포함되어 있는지 확인
-    if type == 1:
-        if f1 == f2:
+def find_parent(x):
+    if parents[x] != x:
+        parents[x] = find_parent(parents[x])
+    return parents[x]
+
+def union(a, b):
+    a = find_parent(a)
+    b = find_parent(b)
+    parents[b] = a
+        
+for _ in range(m):
+    opr, a, b = map(int, input().split())
+    if opr == 0:
+        union(a, b)
+    else:
+        if find_parent(a) == find_parent(b):
             print("YES")
         else:
             print("NO")
-    # type이 0이면 => 집합 합치기
-    else:
-        dot[f2] = f1
-
-n, m = map(int, input().split())
-
-dot = [num for num in range(n+1)]
-
-for _ in range(m):
-    type, n1, n2 = map(int, input().split())
-    union(type, n1, n2)

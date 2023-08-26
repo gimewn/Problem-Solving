c4decbfbd99888350c274901bc7adee22bafe3b3
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-08-27 00:22:37 +0900

    [Gold IV] Title: 친구비, Time: 56 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.py
+++ 백준/Gold/16562. 친구비/친구비.py
@@ -5,17 +5,14 @@ def find(num):
         friend_info[num] = find(friend_info[num])
     return friend_info[num]
 
-def union(a, b, fa, fb):
-    if friend_cost[fa] <= friend_cost[fb]:
-        friend_info[b] = fa
-        for idx in range(1, N+1):
-            if friend_info[idx] == fb:
-                friend_info[idx] = fa
+def union(a, b):
+    fa, fb = find(a), find(b)
+    if fa == fb:
+        return
+    if friend_cost[fa] < friend_cost[fb]:
+        friend_info[fb] = fa
     else:
-        friend_info[a] = fb
-        for idx in range(1, N+1):
-            if friend_info[idx] == fa:
-                friend_info[idx] = fb
+        friend_info[fa] = fb
 
 N, M, K = map(int, sys.stdin.readline().split())
 
@@ -25,12 +22,15 @@ answer = 0
 
 for _ in range(M):
     a, b = map(int, sys.stdin.readline().split())
-    fa, fb = find(a), find(b)
-    if fa != fb:
-        union(a, b, fa, fb)
+    union(a, b)
 
-for friend in set(friend_info):
-    answer += friend_cost[friend]
+friend_set = set()
+
+for friend in friend_info:
+    ff = find(friend)
+    if ff not in friend_set:
+        friend_set.add(ff)
+        answer += friend_cost[ff]
     if answer > K:
         print('Oh no')
         exit(0)

a629ea4875f4e5c3621146d9c643297a98a49fac
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-08-26 23:03:57 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,24 +1,9 @@
 import heapq
 
 def solution(n, costs):
-    def find(num):
-        if num != islands[num]:
-            islands[num] = find(islands[num])
-        return islands[num]
-    
-    def union(a, b):
-        minf, maxf = min(a, b), max(a, b)
-        islands[maxf] = minf
-    
-    answer = 0
-    islands = [num for num in range(n)]
-    costs.sort(key=lambda x : x[2])
-    
+    bridge = [[] for _ in range(n)]
     for a, b, cost in costs:
-        fa, fb = find(a), find(b)
-        if fa == fb:
-            continue
-        union(fa, fb)
-        answer += cost
-        
-    return answer
+        heapq.heappush(bridge[a], (cost, b))
+    
+    print(bridge)
+    return 

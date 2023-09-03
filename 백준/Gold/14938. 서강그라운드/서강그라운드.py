12d7cab981775d7f2885d206d00c0f92ef54bcfb
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-09-04 01:20:27 +0900

    [Gold IV] Title: 서강그라운드, Time: 296 ms, Memory: 33324 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.py
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.py
@@ -1,32 +1,36 @@
-import sys
+import sys, heapq
 
 N, M, R = map(int, sys.stdin.readline().split())
 
 items = [0] + list(map(int, sys.stdin.readline().split()))
-board = [[2e9]*(N+1) for _ in range(N+1)]
-answer = 0
-cal_itemes = [[0]*(N+1) for _ in range(N+1)]
 
-# 자기자신 초기화
-for i in range(1, N+1):
-    board[i][i] = 0
+roads = [[2e9]*(N+1) for _ in range(N+1)]
+
+max_item = 0
 
 for _ in range(R):
-    a, b, length = map(int, sys.stdin.readline().split())
+    a, b, l = map(int, sys.stdin.readline().split())
+    if l > M:
+        continue
+    roads[a][b] = l
+    roads[b][a] = l
 
-    board[a][b] = min(board[a][b], length)
-    board[b][a] = board[a][b]
-
-# 최단거리 갱신
 for i in range(1, N+1):
+    roads[i][i] = 0
+
+for k in range(1, N+1):
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            if roads[i][j] > M and roads[i][k] + roads[k][j] > M:
+                continue
+            if roads[i][j] > roads[i][k] + roads[k][j]:
+                roads[i][j] = roads[i][k] + roads[k][j]
+
+for i in range(1, N+1):
+    count = 0
     for j in range(1, N+1):
-        for k in range(1, N+1):
-            board[j][k] = min(board[j][k], board[j][i] + board[i][k])
-            # 수색 범위 내에 있으면 아이템 줍기
-            if board[j][k] <= M:
-                cal_itemes[j][k] = items[k]
+        if roads[i][j] < 2e9:
+            count += items[j]
+    max_item = max(max_item, count)
 
-for idx in range(1, N+1):
-    answer = max(answer, sum(cal_itemes[idx]))
-
-print(answer)
+print(max_item)

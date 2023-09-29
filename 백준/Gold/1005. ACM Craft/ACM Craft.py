eac6bbdce3f2722af19a4c688e1a104926c83193
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-09-29 22:59:46 +0900

    [Gold III] Title: ACM Craft, Time: 492 ms, Memory: 125444 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -1,46 +1,45 @@
 import sys
-sys.setrecursionlimit(10**9)
 
-def cal_time():
+def cal_min_time():
+    def find_value(index):
+        # 방문한 적 있으면 DP[index] 리턴
+        if DP[index] > -1:
+            return DP[index]
+        # index가 sequence에 존재하지 않으면 딜레이 시간 리턴
+        # ex) 맨 처음 시작점 (1)
+        if index not in sequence:
+            return spend_time[index]
+            
+        # index를 짓기 전 선행되어야 하는 건물들을 차례로 확인
+        for seq in sequence[index]:
+            res = find_value(seq)
+            # 건물 A까지 짓는데 걸리는 시간이 DP[index] 보다 크면 교체
+            if DP[index] < res:
+                DP[index] = res
+        
+        # DP[index]에 index 건물 짓는데 걸리는 시간 더하기
+        DP[index] += spend_time[index]
+        return DP[index]
 
-    def do_seq(now):
-        nonlocal seq
+    n, k = map(int, sys.stdin.readline().split())
+    spend_time = [0] + list(map(int, sys.stdin.readline().split()))
 
-        if DP[now] >= 0:
-            return DP[now]
+    DP = [-1]*(n+1)
 
-        if now not in seq:
-            return time[now]
+    sequence = {}
 
-        for t in seq[now]:
-            res = do_seq(t)
-            if res > DP[now]:
-                DP[now] = res
+    for _ in range(k):
+        x, y = map(int, sys.stdin.readline().split())
+        # 후행 건설되는 건물을 key로 잡기
+        if y not in sequence:
+            sequence[y] = []
+        sequence[y].append(x)
 
-        DP[now] += time[now]
+    w = int(sys.stdin.readline())
 
-        return DP[now]
+    print(find_value(w))
 
-    N, K = map(int, sys.stdin.readline().split())
-    time = [0] + list(map(int, sys.stdin.readline().split()))
-    seq = {}
-    DP = [-1]*(N+1)
+t = int(sys.stdin.readline())
 
-    for _ in range(K):
-        X, Y = map(int, sys.stdin.readline().split())
-        if Y not in seq:
-            seq[Y] = set()
-        seq[Y].add(X)
-
-    W = int(sys.stdin.readline())
-
-    if W in seq:
-        do_seq(W)
-        print(DP[W])
-    else:
-        print(time[W])
-
-T = int(input())
-
-for _ in range(T):
-    cal_time()
+for _ in range(t):
+    cal_min_time()

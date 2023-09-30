27bb73007133c9bd3ea2603fe29ff634d326eca2
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-10-01 01:18:15 +0900

    [Gold V] Title: LCS, Time: 812 ms, Memory: 55712 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,16 +1,16 @@
-def LCS(word1, word2):
-    for i in range(1, len(word1)+1):
-        for j in range(1, len(word2)+1):
-            if word1[i-1] == word2[j-1]:
-                DP[i][j] = DP[i - 1][j - 1] + 1
-            else:
-                DP[i][j] = max(DP[i - 1][j], DP[i][j - 1])
-
 one = input()
 two = input()
 
 DP = [[0]*(len(two)+1) for _ in range(len(one)+1)]
 
-LCS(one, two)
+answer = 0
 
-print(DP[-1][-1])
+for i in range(len(one)):
+    for j in range(len(two)):
+        if one[i] == two[j]:
+            DP[i][j] = DP[i-1][j-1] + 1
+        else:
+            DP[i][j] = max(DP[i-1][j], DP[i][j-1])
+        answer = max(answer, DP[i][j])
+
+print(answer)

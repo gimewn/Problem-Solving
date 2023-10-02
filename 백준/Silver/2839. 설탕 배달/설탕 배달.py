abaa408a0de4150dc1b17e68037b3c2e54449cdc
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-10-02 23:36:25 +0900

    [Silver IV] Title: 설탕 배달, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,23 @@
+sugar = int(input())
+
+result = 0
+
+while sugar >= 0:
+
+    if sugar % 5 == 0:
+
+        result += sugar // 5
+
+        print(result)
+
+        break
+
+    else:
+
+        sugar -= 3
+
+        result += 1
+
+else:
+
+    print(-1)

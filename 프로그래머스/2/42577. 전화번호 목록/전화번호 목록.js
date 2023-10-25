8b8cc9f437e046c0288f2d537d7bcb8b483586fb
Yoonju Kim <97578425+gimewn@users.noreply.github.com>
2023-10-25 14:51:51 +0900

    [level 2] Title: 전화번호 목록, Time: 103.04 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,9 +1,9 @@
 function solution(phone_book) {
     let answer = true;
-    const sortedPhoneBook = phone_book.sort();
+    phone_book.sort();
     
-    sortedPhoneBook.slice(0, sortedPhoneBook.length-1).forEach((item, index) => {
-        if(item === sortedPhoneBook[index+1].slice(0, item.length)){
+    phone_book.slice(0, phone_book.length-1).forEach((item, index) => {
+        if(item === phone_book[index+1].slice(0, item.length)){
             answer = false;
             return;
         }
